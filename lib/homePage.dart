94e11198d6543b71db99ef661231ae89c2500add
Alok Kumax <59159355+alokkumax@users.noreply.github.com>
2020-11-02 12:36:36 +0530

    Update homePage.dart

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pabel/Pabel.dart';
-import 'package:pabel/data.dart';
-import 'package:pabel/model_cate.dart';
-import 'package:pabel/searchBar.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 
 
@@ -29,105 +26,11 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // actions: [
-        //   Tab(
-        //     icon: Image.asset("images/git.png",
-        //     height: 38,
-        //     width: 38,),
-        //   )
-        // ],
-        title: PabelLogo(),
+        title: SampleLogo(),
       ),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-
-                  Container(
-                    alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Text("Popular Searches",style: GoogleFonts.inter(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600,
-                        ),),
-                      ))
-                ],
-              ),
-              // SizedBox(height: 5,),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Container(
-                height: 80,
-                child: ListView.builder(
-                    itemCount: categories.length,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                    itemBuilder:(context, index){
-                      return CategoriesTile(
-                        title:categories[index].categoriesName ,
-                        imgUrl: categories[index].imgUrl,
-                      );
-                    }),
-              ),
-            ),
-              Container(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Text("Trending Today",style:GoogleFonts.inter(
-                      color: Colors.black,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600,
-                    ),),
-                  ))
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: searchBar(),
+      body: ////
+    
     );
   }
 }
-
-class CategoriesTile extends StatelessWidget {
-  String imgUrl;
-  String title;
-  CategoriesTile({@required this.title,@required this.imgUrl});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-
-      child: Stack(
-        children: <Widget>[
-          Container(
-
-          margin: EdgeInsets.only(right: 5),
-            child: Image.network(imgUrl,
-            height: 120,
-            width: 160,
-            fit: BoxFit.cover),
-          ),
-          Center(
-            child: Container(
-              width: 160,
-              alignment: Alignment.center,
-              child: Text(title,  style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 23,
-                  fontWeight: FontWeight.w500,
-                  ),),
-            ),
-          ),
-
-        ],
-      ),
-    );
-  }
-}
-
